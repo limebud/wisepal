e835a1242b574a844711fc2e677ef395e24aab31
Jonatan Lindblom <gregcompu@hotmail.com>
2018-09-27 09:32:16 +0200

    :

---
+++ app/components/Customer/CustomerDocuments.vue
@@ -0,0 +1,31 @@
+<template>
+  <StackLayout>
+      <ScrollView>
+            <ListView class="list-group" for="document in documents" style="height:1250px">
+              <v-template>
+                <FlexboxLayout flexDirection="row" class="list-group-item">
+                  <Label :text="document.Title.Value" class="list-group-item-heading" style="width: 60%"/>
+                </FlexboxLayout>
+              </v-template>
+            </ListView>
+      </ScrollView>
+  </StackLayout>
+</template>
+
+<script>
+  export default {
+      name: "customer-documents",
+      data() {
+          return {
+              documents: null
+          }
+      },
+      created() {
+          this.documents = this.$store.getters.getCustomerDocuments
+
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ app/components/Customer/CustomerInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <GridLayout rows="*, 4*, *, 3*">
+      <StackLayout row="1"  class="header">
+        <Image src="~/images/userphoto@2x.png" class="img" />
+      </StackLayout>
+      <StackLayout row="3">
+        <Label :text="customerInfo.Name.Value" class="name" />
+        <Label :text="customerInfo.SocialSecurityNumber.Value" />
+      </StackLayout>
+</GridLayout>
+</template>
+
+<script>
+  export default {
+      name: "customer-info",
+      data() {
+          return {
+              customerInfo: []
+          }
+      },
+      created() {
+          this.customerInfo = this.$store.getters.getCustomerInformation
+      }
+  }
+</script>
+
+<style scoped lang="scss">
+GridLayout {
+    text-align: center;
+}
+
+.img  {
+    margin: 30px;
+}
+
+.name {
+    font-size: 20vw;
+    font-weight: bold;
+}
+
+.id {
+    font-size: 15vw;
+}
+</style>
