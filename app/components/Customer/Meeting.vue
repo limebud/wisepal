c4e4c6878267b846f61346aa214426e5122cb9b5
Jonatan Lindblom <gregcompu@hotmail.com>
2018-10-05 10:55:21 +0200

    ta kort

--- app/components/Customer/Meeting.vue
+++ app/components/Customer/Meeting.vue
@@ -3,19 +3,46 @@
       <GridLayout columns="*, auto, *" class="tasks">
           <FlexboxLayout col="1">
               <Label class="fas" :text="'fa-pen' | fonticon" col="1" />
-              <Label class="fa" :text="'fa-camera' | fonticon" col="1" />
+              <Label class="fa" :text="'fa-camera' | fonticon" col="1" @tap="useCamera"/>
               <Label class="fa" :text="'fa-microphone' | fonticon" col="1" />
           </FlexboxLayout>
+          <StackLayout>
+              <Image v-if="image" :src="image.src" />
+          </StackLayout>
       </GridLayout>
   </StackLayout>
 </template>
 
 
 <script>
+  import * as camera from "nativescript-camera";
+  import { Image } from "ui/image";
+
   export default {
       name: "meeting",
       data() {
           return {
+              image: null
+          }
+      },
+      methods: {
+          useCamera() {
+            if (camera.isAvailable()) {
+                    
+                camera.requestPermissions()
+                .then(() => {
+                    camera.takePicture()
+                })
+                .then((imageAsset) => {
+                    console.log("Result is an image asset instance");
+                    var image = new Image();
+                    image.src = imageAsset;
+                    console.log("IMAGE SOURCE: " + image)
+                    this.image = image.src
+                }).catch((err) => {
+                    console.log("Error -> " + err.message);
+                });
+            }
           }
       }
     }
